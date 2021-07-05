cac7df87b1ae17da075167a10e7296e9bcaf6f22
Federico <federico.castanares@gmail.com>
2021-07-05 14:17:57 -0300

    cambio en el id del celular

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -15,13 +15,13 @@ const Products = () => {
         <Grid item xs={12} sm={6} md={3} bg={2}>
           <Product product={{
               title: "Samsung S20",
-              description: "Dispositivo móvil de Tienda e-commerce",
+              description: "Dispositivo movil de Tienda e-commerce",
               thumbnail: 'https://d391ci4kxgasl8.cloudfront.net/fit-in/524x480/filters:fill(FFFFFF):quality(90):format(webp)/_img_productos/samsung-galaxy-s20fe-preventa-fit2-foto-lavanda2.jpg',
               // category_id: product.category_id,
               price: 45000,
               //currency_id: product.currency_id,
               quantity: 1,
-              id: 1234,
+              id: "1234",
               prices: {
                 prices: [{
                   amount: 45000

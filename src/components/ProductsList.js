849b73699550fc85cd36389b0fb160863d83b29c
Federico <federico.castanares@gmail.com>
2021-08-08 16:40:17 -0300

    sacando producto para prueba

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -12,28 +12,15 @@ const Products = () => {
         Productos
       </Typography>
       <Grid container>
-        <Grid item xs={12} sm={6} md={3} bg={2}>
-          <Product product={{
-              title: "Samsung Galaxy S9",
-              description: "Dispositivo móvil de Tienda e-commerce",
-              thumbnail: 'https://d391ci4kxgasl8.cloudfront.net/fit-in/524x480/filters:fill(FFFFFF):quality(90):format(webp)/_img_productos/samsung-galaxy-s20fe-preventa-fit2-foto-lavanda2.jpg',
-              price: 45000,
-              quantity: 1,
-              id: "1234",
-              prices: {
-                prices: [{
-                  amount: 45000
-                }]
-              }
-            }}
-          />
-        </Grid>
-        {products &&
+        {products ?
           products.products.results.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} md={3} bg={2}>
               <Product product={product} />
             </Grid>
-          ))}
+          )):
+          <Typography variant="body1" component="p" align="center">
+            No hay productos
+          </Typography>}
       </Grid>
     </>
   );
